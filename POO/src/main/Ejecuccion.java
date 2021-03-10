e9011ff787d915f6eae9ddd1a22cbedd762f69dd
a.lopez <a.lopez@1DAW-20.2SMR.GSD.local>
2021-03-10 12:51:15 +0100

    Añadir segundo proyecto

---
+++ POO/src/main/Ejecuccion.java
@@ -0,0 +1,15 @@
+package main;
+
+import persona.viajero.viajero;
+
+public class Ejecuccion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 viajero v1 = new viajero("123456","Javi","Sanz","Espa","Pedre");
+		 v1.getInfo();
+		 viajero v2 = new viajero ();
+		 v2.getInfo();
+	}
+
+}
